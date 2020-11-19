511e689dc545f6d32eecd266d884704291bee754
2019111101 <2019111101@FINPC-250.tejwin.com>
2020-11-19 17:18:22 +0800

    Q28

---
+++ LeetCode/src/problems/Q28.java
@@ -0,0 +1,24 @@
+package problems;
+
+public class Q28 {
+
+	public static void main(String[] args) {
+		System.out.println(strStr("abcd123", "123") );
+	}
+
+	//思考 : 實做indexOf : 順序的從haystack 取 needle長度的子字串去做比對
+	public static int strStr(String haystack, String needle) {
+
+        if(needle.length() == 0) return 0;
+		char[] array = haystack.toCharArray();
+		for(int i=0; i < haystack.length() - needle.length() +1 ; i++)
+		{
+			if(needle.equals(haystack.substring(i, i + needle.length())))
+			{
+				return i;
+			}
+		}
+		
+		return -1;
+    }
+}
