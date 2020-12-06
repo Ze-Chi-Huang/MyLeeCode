904dc210a907c39c556c507a7f01ee1bf27f2094
Ze-Chi <lo4251054@gmail.com>
2020-12-06 14:46:17 +0800

    Q50 fixed

--- LeetCode/src/problems_medium/Q50.java
+++ LeetCode/src/problems_medium/Q50.java
@@ -4,9 +4,56 @@ public class Q50 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(myPow(2,-2147483648));
-		Math.pow(1, 1);
+		long start = System.currentTimeMillis();
+//		System.out.println(myPow2(2,-2147483648));
+		System.out.println(pow(5,6));
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
 	}
+	
+	//思考 : 分割法，每一次都分成2半去相乘，最後再乘起來(迴圈)
+	public static double pow(double x,int n){
+		if(n ==0) return 1;
+		double res = 1;
+		
+		while(n > 0) {
+			if(n % 2 !=0) {
+				res *= x;
+			}
+			x *= x;
+			n = n/2;
+		}
+		
+		return res;
+	}
+	
+	//思考 : 分割法，每一次都分成2半去相乘，最後再乘起來(遞迴)
+	public static double myPow2(double x, int n) {
+
+		if(n>0) {
+			return pow2(x,n);
+		}else {
+			return 1/pow2(x,n);
+		}
+		
+	}
+	
+	/*不斷的分解 n 
+	Ex. 5 ^ 3
+	=  5^1 * 5^1 ^ 5 
+	*/
+	public static double pow2(double x, int n) {
+		if(n == 0) return 1;
+
+		double y = pow2(x,n/2); //思考: 一直除以2，直到為0 
+		if( n % 2 ==0) {
+			return y*y;
+		}else {
+			return y*y*x;
+		}
+	}
+	
+	//太久了!
 	public static double myPow(double x, int n) {
 		//思考 : n = 0 -> 1, n > 0 -> n 方  ,n < 0 -> 1/ n 方
 		if(n == 0 || x == 1) return 1;
