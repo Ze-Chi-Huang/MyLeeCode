726134f75a8d84f1d75402cd396074f61d96dfbc
Ze-Chi <lo4251054@gmail.com>
2020-12-06 23:33:20 +0800

    Q11 temp

---
+++ LeetCode/src/problems_medium/Q11.java
@@ -0,0 +1,17 @@
+package problems_medium;
+
+public class Q11 {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+		
+		System.out.println("height: " + maxArea(height));
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+	
+	public static int maxArea(int[] height) {
+		return 0;
+    }
+}
