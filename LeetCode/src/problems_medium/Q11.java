be9b6afe0d59931c75a7976813328397336b7516
Ze-Chi <lo4251054@gmail.com>
2020-12-27 22:49:14 +0800

    Q11

--- LeetCode/src/problems_medium/Q11.java
+++ LeetCode/src/problems_medium/Q11.java
@@ -4,14 +4,35 @@ public class Q11 {
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+//		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+		int[] height = new int[] {4,3,2,1,4};
+		
 		
 		System.out.println("height: " + maxArea(height));
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
 	}
 	
+	// 思考 : 決定長寬，再找最大值
+	// 最大面積 : MAX
 	public static int maxArea(int[] height) {
-		return 0;
+		if(height.length == 0) return 0;
+		
+		int max = 0;
+		for(int i=0; i<height.length; i++) {
+			
+			for(int j = i; j<height.length; j++) {
+				
+				int length = j-i;
+				int width = Math.min(height[i], height[j]);
+				int area = length * width;
+				if(area > max) {
+					max = area;
+				}
+				
+			}
+		}
+		
+		return max;
     }
 }
