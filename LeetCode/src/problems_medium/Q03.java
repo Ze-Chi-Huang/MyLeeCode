38976861674be637ebf7b4245f0b77d76de81585
2019111101 <2019111101@FINPC-250.tejwin.com>
2021-05-03 14:30:43 +0800

    Q03

--- LeetCode/src/problems_medium/Q03.java
+++ LeetCode/src/problems_medium/Q03.java
@@ -4,13 +4,39 @@ public class Q03 {
 
 	public static void main(String[] args) {
 		
-		String s = "abcabcbb";
+		String s = " ";//pwwkew
 		System.out.println(lengthOfLongestSubstring(s));
 	}
 	
+	//直觀思考 : 字串不重複 + 子字串
 	public static int lengthOfLongestSubstring(String s) {
 		//找最長的子字串
-		//思考 : 從第一個
-		return 0;
+		if(s == null || s.equals("")) return 0;
+		
+		
+		char[] charArray = s.toCharArray();
+		
+		int maxLen = 0;
+		
+		for(int n=0; n<charArray.length;n++) {
+			String subStr = "";
+			for(int i=n; i< charArray.length; i++) { 
+				//條件1 : 不重複
+				if(!subStr.contains(Character.toString(charArray[i]))) {
+					
+					subStr += charArray[i];
+					if(subStr.length() > maxLen) maxLen = subStr.length();
+					//也可用指標代替:
+//					if(i-n+1 > maxLen) maxLen = i-n+1;
+					
+				}else {
+					//條件2 : 子字串
+					break;
+				}
+			}
+		}
+		
+		return maxLen;
 	}
+
 }
